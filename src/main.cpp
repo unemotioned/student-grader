7f50867b2f074a1c118c2bb7f779ac7b8b2e5561
unemotioned <blackeagle10@icloud.com>
2026-04-14 13:25:46 +0900

    format: LLVM style, indentWidth w/ 4 spaces

--- src/main.cpp
+++ src/main.cpp
@@ -14,49 +14,49 @@ using std::list;
 using std::string;
 
 class Student {
- public:
-  int num;
-  string name;
-  int kor;
-  int eng;
-  int math;
-  int tot;
-  float avg;
-  char grade;
-  int rank;
+  public:
+    int num;
+    string name;
+    int kor;
+    int eng;
+    int math;
+    int tot;
+    float avg;
+    char grade;
+    int rank;
 };
 
 int main() {
-  // Read file
-  ifstream studentFile;
-  studentFile.open("./student_scores.txt");
+    // Read file
+    ifstream studentFile;
+    studentFile.open("./student_scores.txt");
 
-  // Create student class
-  Student student;
-  list<Student> students;
+    // Create student class
+    Student student;
+    list<Student> students;
 
-  // Assign contents of file to string variable
-  string studentInfo = "";
+    // Assign contents of file to string variable
+    string studentInfo = "";
 
-  // read from studentFile
-  // for each line
-  // assign it to studentInfo
-  while (getline(studentFile, studentInfo)) {
-    istringstream iss(studentInfo);
-    iss >> student.num >> student.name >> student.kor >> student.eng >>
-        student.math;
+    // read from studentFile
+    // for each line
+    // assign it to studentInfo
+    while (getline(studentFile, studentInfo)) {
+        istringstream iss(studentInfo);
+        iss >> student.num >> student.name >> student.kor >> student.eng >>
+            student.math;
 
-    student.tot = student.kor + student.eng + student.math;
-    student.avg = static_cast<float>(student.tot) / 3;
+        student.tot = student.kor + student.eng + student.math;
+        student.avg = static_cast<float>(student.tot) / 3;
 
-    students.push_back(student);
-    studentInfo = "";
-  }
-  studentFile.close();
+        students.push_back(student);
+        studentInfo = "";
+    }
+    studentFile.close();
 
-  for (Student student : students) {
-    cout << student.name << " - average score: " << student.avg << endl;
-  }
+    for (Student student : students) {
+        cout << student.name << " - average score: " << student.avg << endl;
+    }
 
-  return 0;
+    return 0;
 }
